7e6f9ce95c1d4ab90f827514ad318cf21701a5e8
tanimuraazuma <azumax3252@yahoo.co.jp>
2020-02-25 23:51:35 +0900

    create_manythings

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
 
   def index
     @tweets = Tweet.all
@@ -7,4 +8,37 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
+
+  def create
+    Tweet.create(tweet_params)
+    redirect_to action: :index
+  end
+
+  def show  
+  end
+
+  def edit
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to action: :index
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:content, :image)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
+  devise_for :users
   root to: 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tweets, only: [:index, :new, :create, :edit, :destroy, :update]
+  resources :tweets
 end
