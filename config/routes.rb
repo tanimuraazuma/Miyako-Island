4fff1b04dd885172b87ee802fd013fc81430357b
tanimuraazuma <azumax3252@yahoo.co.jp>
2020-02-26 21:29:31 +0900

    create_shops

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   
+  get 'shops/index'
   devise_for :users
   root to: 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tweets
   resources :users, only: :show
+  resources :shops, only: :index
+
 
 end
